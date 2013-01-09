b26caf5ae778dc02ce2bfa881cf17ae745e4dc28
devona <dward1289@fullsail.edu>
2013-01-09 14:49:07 -0500

    First 3 tasks competed

--- projectOne/projectOne/AppDelegate.m
+++ projectOne/projectOne/AppDelegate.m
@@ -23,6 +23,7 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
+    //First task: Create a variable using the float data type. Cast the float to an int and using NSLog, output both the initial float value as well as the int value.
     //Integers for work hours and total appointments.
     int appointments = 20;
     int shiftHours = 8;
@@ -36,6 +37,53 @@
     
     //NSLog showing the total of appointments divided by shiftHours.
     NSLog(@"Total breaks recommended from 20 appointments within 8 hours: %f breaks", total);
+    //First task ended
+    
+    
+    //Second task: Perform an AND, OR comparison. Use float, int and BOOL types. BOOL values should be YES or NO and written in all caps.
+    //James and Mike both have devices. Their names equal the number of devices. Only Mike will owe money.
+    int James = 2;
+    int Mike = 1;
+    float moneyOwe = 153.95;
+    
+    //If Mike is here to pickup his device, then isTrue will execute.
+    BOOL isTrue = ((Mike >= 1) || (moneyOwe <= 200));
+    if(isTrue){
+        NSLog(@"James needs an appointment for each device. He has %d devices.", James);
+        NSLog(@"Mike is here to pickup his Mac.");
+        NSLog(@"He had his hard drive and display replaced. Mike has a balance of %f that must be paid before he leaves.", moneyOwe);
+              }
+    
+    //If James has an appointment, then Mike's device isn't ready. isFalse will execute.
+    BOOL isFalse = ((James <= 1) && (moneyOwe >= 300));
+    if(isFalse){
+        NSLog(@"James has an appointment.");
+        NSLog(@"Mike can not pickup his Mac because repairs are still in progress. His current balance is %f and can be paid now, or at pickup.", moneyOwe);
+    }
+    //Second task ended
+    
+    
+    //Third task: Use an if, else if and else check using any of the data types of your choice.
+    //If there is liquid inside the device, the scale is at a max of 6. If the device is cracked, the scale is at a max of 5.
+    int liquidDmg = 6;
+    int crackDmg = 5;
+    
+    NSLog(@"My next customer has a few problems with his device.");
+    
+    if((liquidDmg > 0) && (crackDmg == 0))
+    {
+        NSLog(@"The device has liquid damage. There will be a charge for a replacement.");
+    }
+    else if ((crackDmg > 0) && (liquidDmg > 0))
+    {
+        NSLog(@"The device has cracks and liquid damage. There will be a charge for a replacement.");
+    }
+    else
+    {
+        NSLog(@"It seems may be a few software issues.");
+    }
+    
+    //Third task ended
     
     
     return YES;
