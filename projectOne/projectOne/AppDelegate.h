dfa96667d07ea268bf0a3a9322c2af4b86156d92
devona <dward1289@fullsail.edu>
2013-01-09 09:31:59 -0500

    Files added

---
+++ projectOne/projectOne/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  projectOne
+//
+//  Created by Devona Ward on 1/9/13.
+//  Copyright (c) 2013 Devona Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
